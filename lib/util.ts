3d57fb3c3882cb373614318aeaab12cc5db2bf6a
TonyLiu0430 <tony04302020@gmail.com>
2024-11-09 00:02:50 +0800

    feat: implement email login verification and token generation

---
+++ lib/util.ts
@@ -0,0 +1,4 @@
+export default function isVaildEmail(email: string) {
+    const emailRegex = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+    return emailRegex.test(email)
+}
