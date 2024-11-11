dd562b2c49b70cbc5cfa7c551ef390fee463e34f
TonyLiu0430 <tony04302020@gmail.com>
2024-11-12 01:31:11 +0800

    feat: implement user update endpoint

---
+++ server/routes/user/[userId].put.ts
@@ -0,0 +1,62 @@
+import prisma from '~~/lib/prisma';
+import { isVaildEmail } from '~~/lib/util';
+
+export default defineEventHandler(async (event) => {
+    const userIdStr = getRouterParam(event, 'userId');
+
+    if (userIdStr == undefined || isNaN(parseInt(userIdStr)) || isNaN(Number(userIdStr))) {
+        throw createError({
+            statusCode: 400,
+            message: 'Invalid userId'
+        })
+    }
+
+    const userId = parseInt(userIdStr);
+
+    if(event.context.userId != userId){
+        throw createError({
+            statusCode: 403,
+            message: 'Forbidden'
+        })
+    }
+
+    const { name, email } = await readBody(event) as {
+        name: string | undefined,
+        email: string | undefined
+    }
+
+    if (name == undefined || email == undefined) {
+        throw createError({
+            statusCode: 400,
+            message: 'parameter name and email is required'
+        })
+    }
+
+    //test name 特殊字元
+    if (/[|&;$%@"<>()+,'"]/.test(name)) {
+        throw createError({
+            statusCode: 400,
+            message: 'Invalid name'
+        })
+    }
+
+    if (isVaildEmail(email) == false) {
+        throw createError({
+            statusCode: 400,
+            message: 'Invalid email'
+        })
+    }
+
+    await prisma.user.update({
+        where: {
+            id: userId
+        },
+        data: {
+            name,
+            email
+        },
+        select: {
+            id: true
+        }
+    })
+});
